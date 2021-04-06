09029f0bfac777b10121eb1ec116f15339995f54
bhupendra1988 <bhupendra.varanasi@gmail.com>
2021-04-06 23:19:16 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0742b4e673072066f"
   instance_type = "t2.micro"
   key_name      = "devops"
-  tags = {
-    Name = "terraform-machine"
-  }
+#  tags = {
+#    Name = "terraform-machine"
+#  }
 }
